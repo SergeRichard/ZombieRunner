b22b0a341bd8fab39275ede8a493c0fd7d5caa76
SergeRichard <serge.richard@rogers.com>
2016-07-16 19:36:56 -0300

    Added player spawn points.

---
+++ Assets/Player.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public Transform PlayerSpawnPoints;
+	public bool ReSpawn = false;
+
+	private bool lastToggle = false;
+	private Transform[] SpawnPoints;
+
+	// Use this for initialization
+	void Start () {
+		SpawnPoints = PlayerSpawnPoints.GetComponentsInChildren<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (lastToggle != ReSpawn) {
+			Respawn ();
+			ReSpawn = false;
+		} else {
+			lastToggle = ReSpawn;
+		}
+	}
+	private void Respawn() {
+		int i = Random.Range (1, SpawnPoints.Length);
+		transform.position = SpawnPoints [i].transform.position;
+
+	}
+}
