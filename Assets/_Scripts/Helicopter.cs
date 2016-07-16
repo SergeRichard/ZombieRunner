55c2b4c95688154fb6b2f9e64fdb71c2b5b97c7a
SergeRichard <serge.richard@rogers.com>
2016-07-16 20:01:27 -0300

    Added "Zoom" key input that zooms while pressed down.

---
+++ Assets/_Scripts/Helicopter.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Helicopter : MonoBehaviour {
+
+	public AudioClip CallSound;
+
+	private bool called = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("CallHeli") && !called) {
+			called = true;
+			//Play sound
+
+		} 
+	}
+}
